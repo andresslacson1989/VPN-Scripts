3a30ece236ac24df97afd534b8a1370baa4051d3
andresslacson1989 <phcyber2018@gmail.com>
2023-04-04 14:37:03 +0800

    updated files

--- go_connect.sh
+++ go_connect.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-source ~/.db-base
 
-datenow=`date +"%Y-%m-%d %T"`
-
-read user_name is_freeze duration <<< $(MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME -sNe "SELECT user_name, is_freeze, duration FROM users WHERE user_name='$USERNAME'")
-
-if [[ $is_freeze -eq 1 ]]; then
-exit -1
-else
-    if [[ $duration -eq 0 ]]; then
-    exit -1
-    else
-        MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME  -sse "UPDATE users SET is_connected='1', active_address='LENZPOGI', active_date='$datenow', device_connected='1' WHERE user_name='$USERNAME'"
-        MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME  -sse "UPDATE server_list SET online=online+1 WHERE server_ip='LENZPOGI'"
-    fi
-fi
+##set status online to user connected
+server_ip=LENZPOGI
+curl -sb -X POST DOMAIN/api/server/update-status -H "Content-Type: application/x-www-form-urlencoded" -d "status=1&username=$common_name&ip=$server_ip"
--- go_disconnect.sh
+++ go_disconnect.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source ~/.db-base
-MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME  -sse "UPDATE users SET is_connected='0', active_address='', active_date='' WHERE user_name='$USERNAME'"
-MYSQL_PWD=$DB_PASS mysql -u $DB_USER -h $DB_HOST -D $DB_NAME  -sse "UPDATE server_list SET online=online-1 WHERE server_ip='LENZPOGI'"
+
+curl -sb -X POST DOMAIN/api/server/update-status -H "Content-Type: application/x-www-form-urlencoded" -d "status=0&username=$common_name"
